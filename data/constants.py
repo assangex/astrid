74d0988be57ff84a3775b0e219bbea35e5e9c7cc
sys34245 <sabir.fisher@outlook.com>
2024-09-10 01:13:25 -0500

    Add files via upload

--- data/constants.py
+++ data/constants.py
@@ -1,4 +1,6 @@
 #constants.py
-
+#Add as many constants as you need to this file.
+LIM = 4 # number of decimal places to round to
 GRAVITY = 9.81 # m/s^2
-C_DRAG = 0.55 # drag coefficient
+C_DRAG = 0.45 # drag coefficient
+
